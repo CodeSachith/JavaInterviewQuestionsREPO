4808364f5dab9c74e071c244794baa203b3305e6
Sachith <sachithsenevirathna@yahoo.com>
2019-02-03 03:17:09 +0530

    first commit

---
+++ src/q1/ReverseString.java
@@ -0,0 +1,19 @@
+package q1;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		
+		String text = "Selenium";
+		
+		int len= text.length();
+		
+		for(int i=len-1;i>0;i--) {
+			
+			text.charAt(i);
+			
+		}
+	}
+
+}
